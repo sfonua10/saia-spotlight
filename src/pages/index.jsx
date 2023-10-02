da7ea73af0d37a0f490426e24eded06bf91080bc
Saia Fonua <saiafonua@gmail.com>
2023-10-02 13:25:22 -0600

    update meta content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -304,8 +304,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Saia, a software designer and entrepreneur based in Lehi, Utah. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
-        />
+          content="I’m Saia, a software developer based in Lehi, Utah with a knack for crafting innovative apps and intriguing/interesting dishes. Balancing tech and health, always learning, always growing."        />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
